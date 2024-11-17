85e1e832eef5c8a7ed2d9e80c8d2dd9794de6a65
gyanqui <gyanqui3@gmail.com>
2024-11-16 21:50:48 -0500

    W02 Project: Contacts Part 2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
+
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => { res.send('Hello world');});
 
 router.use('/users', require('./users'));
-
 
 module.exports = router;
